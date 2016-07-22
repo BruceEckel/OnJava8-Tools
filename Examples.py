01b7741e75d1deeec22d5480d107dcfaec35b718
Bruce Eckel <BruceTEckel@gmail.com>
2016-07-22 17:27:35 -0600

    Incorporated phase1.py into test_output.py

--- Examples.py
+++ Examples.py
@@ -30,7 +30,6 @@ gradle_files = Path(sys.path[0]).parent / "OnJava-Examples"
 tools_to_copy = [ Path(sys.path[0]) / f for f in [
     "output_duet.py",
     "verify_output.py",
-    "phase1.py",
     "test_output.py",
     "update_extracted_example_output.py",
 ]]
