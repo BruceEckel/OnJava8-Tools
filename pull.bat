72be2c4846487c468d6a43ccddcbc09c6e0634b6
Bruce Eckel <mindviewinc@gmail.com>
2016-08-18 10:21:25 -0600

    Check computer name

--- pull.bat
+++ pull.bat
@@ -1,6 +1,15 @@
+if %COMPUTERNAME% == TEENYVERSE (
+cd C:\Users\bruce\Documents\Git\OnJava-Tools
+git pull
+cd C:\Users\bruce\Documents\Git\OnJava-Examples
+git pull
+cd C:\Users\bruce\Documents\Git\on-java
+git pull
+) Else (
 cd C:\Users\Bruce\Documents\GitHub\OnJava-Tools
 git pull
 cd C:\Users\Bruce\Documents\GitHub\OnJava-Examples
 git pull
 cd C:\Users\Bruce\Documents\GitHub\on-java
 git pull
+)
