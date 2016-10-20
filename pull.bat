fc9121f06e0ddcdcfbf44493d5197d07c2126229
Bruce Eckel <mindviewinc@gmail.com>
2016-10-20 11:27:39 -0700

    Change to using environment variable

--- pull.bat
+++ pull.bat
@@ -1,16 +1,27 @@
 %echo off
-if %COMPUTERNAME% == TEENYVERSE (
-cd C:\Users\bruce\Documents\Git\OnJava-Tools
-git pull
-cd C:\Users\bruce\Documents\Git\OnJava-Examples
-git pull
-cd C:\Users\bruce\Documents\Git\on-java
-git pull
-) Else (
-cd C:\Users\Bruce\Documents\GitHub\OnJava-Tools
-git pull
-cd C:\Users\Bruce\Documents\GitHub\OnJava-Examples
-git pull
-cd C:\Users\Bruce\Documents\GitHub\on-java
-git pull
+if "%GIT_HOME%"=="" (
+  echo Please set GIT_HOME
+  exit /B
 )
+cd %GIT_HOME%\OnJava-Tools
+git pull
+cd %GIT_HOME%\OnJava-Examples
+git pull
+cd %GIT_HOME%\on-java
+git pull
+
+rem if %COMPUTERNAME% == TEENYVERSE (
+rem cd C:\Users\bruce\Documents\Git\OnJava-Tools
+rem git pull
+rem cd C:\Users\bruce\Documents\Git\OnJava-Examples
+rem git pull
+rem cd C:\Users\bruce\Documents\Git\on-java
+rem git pull
+rem ) Else (
+rem cd C:\Users\Bruce\Documents\GitHub\OnJava-Tools
+rem git pull
+rem cd C:\Users\Bruce\Documents\GitHub\OnJava-Examples
+rem git pull
+rem cd C:\Users\Bruce\Documents\GitHub\on-java
+rem git pull
+rem )
