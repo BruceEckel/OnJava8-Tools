929eef2cba2ee720d9b23c12411e3a6a94dff757
Bruce Eckel <mindviewinc@gmail.com>
2016-10-20 11:43:06 -0700

    Cleanup

--- pull.bat
+++ pull.bat
@@ -9,19 +9,3 @@ cd %GIT_HOME%\OnJava-Examples
 git pull
 cd %GIT_HOME%\on-java
 git pull
-
-rem if %COMPUTERNAME% == TEENYVERSE (
-rem cd C:\Users\bruce\Documents\Git\OnJava-Tools
-rem git pull
-rem cd C:\Users\bruce\Documents\Git\OnJava-Examples
-rem git pull
-rem cd C:\Users\bruce\Documents\Git\on-java
-rem git pull
-rem ) Else (
-rem cd C:\Users\Bruce\Documents\GitHub\OnJava-Tools
-rem git pull
-rem cd C:\Users\Bruce\Documents\GitHub\OnJava-Examples
-rem git pull
-rem cd C:\Users\Bruce\Documents\GitHub\on-java
-rem git pull
-rem )
