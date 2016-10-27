7c55e5d12ccc03091e6a32c34f3d23a88657e9bc
Bruce Eckel <BruceTEckel@gmail.com>
2016-10-26 19:47:33 -0700

    Progress

--- reformat_markdown.py
+++ reformat_markdown.py
@@ -2,6 +2,11 @@
 Reformats Pandoc-flavored Markdown, preserving hyphen cuddling
 """
 import textwrap
+import string
+
+marker_chars = ">!#<"
+
+subhead_chars = string.ascii_letters + string.digits + "`"
 
 def reformat_paragraph(text, width = 80):
     "Reformat a single paragraph that is not-code or a subhead"
@@ -19,15 +24,51 @@ def reformat_paragraph(text, width = 80):
       text = text.replace(" -", "-")
     return formatter.fill(text)
 
-def reformat_document(doc_text, width = 80):
+
+class ReformatDocument:
     """
     Reformat an entire document, but only the paragraphs,
     not code or subheads or any other markup.
     """
-    result = []
-    lines = doc_text.split()
-    n = 0
-    while n < len(lines):
+    def __init__(self, doc_text, width = 80):
+        self.result = []
+        self.lines = doc_text.split()
+        self.n = 0
+
+    def at_least_one(self, offset=0):
+        index = self.n + offset
+        if index >= len(self.lines):
+            return False
+        if len(self.lines[index].strip()) == 0
+            return False
+        return True
+
+    def at_least_two(self):
+        return at_least_one() and at_least_one(1)
+
+    def absorb(self, num_of_lines):
+        for i in range(num_of_lines):
+            self.result.append(self.lines[self.n])
+            self.n += 1
+
+    def skip_marked_line(self):
+        if self.at_least_one() and self.lines[n][0] in marker_chars:
+            self.absorb(1)
+            return True
+        return False
+
+    def skipsubhead(self):
+        if not self.at_least_two():
+            return False
+        if self.lines[self.n][0] in subhead_chars and self.lines[self.n + 1][0] in "-=":
+            self.absorb(2)
+            return True
+        return False
+
+    def start(self):
+        while n < len(self.lines):
+            if self.skip_marked_line(): continue
+            if self.skipsubhead(): continue
 
 
 
