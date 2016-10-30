d85d360c573e27cc20eca862eaa488c8446ecfbe
Bruce Eckel <BruceTEckel@gmail.com>
2016-10-30 00:12:35 -0700

    Big structural improvements

--- reformat_markdown.py
+++ reformat_markdown.py
@@ -6,12 +6,6 @@ Doesn't touch code listings.
 Has the option to fix other issues as well, such as the underlines for section
 headings and ensuring there are blank lines after various items.
 
-TODO: Create class that is just for managing lines in the doc, possibly called
-Index or Lines. Keeps track of where you are, whether a line is blank or has
-characters, whether the following line is blank or has characters, if you're at
-the end, if you have a next line. You explicitly tell it to move the index
-forward and at that point it calculates everything about where it is.
-
 """
 import textwrap
 import string
@@ -20,99 +14,132 @@ marker_chars = ">!#<"
 
 subhead_chars = string.ascii_letters + string.digits + "`"
 
-def reformat_paragraph(text, width = 80):
-    "Reformat a single paragraph that is not-code or a subhead"
-    formatter = textwrap.TextWrapper(
-        width = width,
-        break_long_words = False,
-        break_on_hyphens = False,
-      )
-    text = text.strip().replace("\n", " ")
-    while text.find("  ") is not -1:
-      text = text.replace("  ", " ")
-    while text.find("- ") is not -1:
-      text = text.replace("- ", "-")
-    while text.find(" -") is not -1:
-      text = text.replace(" -", "-")
-    return formatter.fill(text)
+
+class MarkdownLines:
+    """
+    Manages the lines from a Markdown file
+    """
+    def __init__(self, doc_text):
+        self.index = 0
+        self.lines = []
+        self.result = []
+        for line in doc_text.split():
+            self.lines.append(line.rstrip())
+        self.size = len(self.lines)
+        self.eof = False
+
+    def eof(self): return self.eof
+
+    def line(self): return self.lines[self.index]
+
+    def blank(self): return len(self.line()) is 0
+
+    def nonblank(self): return len(self.line()) line not 0
+
+    def next_line(self):
+        if self.eof():
+            raise IndexError("next_line() out of range")
+        return self.lines[self.index + 1]
+
+    def next_line_blank(self): return len(self.next_line()) is 0
+
+    def next_line_nonblank(self): return len(self.next_line()) is not 0
+
+    def increment(self):
+        if self.index < self.size:
+            self.index += 1
+        else:
+            self.eof = True
+
+    def transfer(self, count = 1):
+        for i in range(count):
+            if not self.eof:
+                self.result.append(self.line())
+                self.increment()
 
 
-class ReformatDocument:
+class ReformatMarkdownDocument(MarkdownLines):
     """
     Reformat an entire document, but only the paragraphs,
     not code or subheads or any other markup.
     """
     def __init__(self, doc_text, width = 80):
-        self.result = []
-        self.lines = doc_text.split()
-        self.n = 0
+        super().__init__(doc_text)
+        self.formatter = textwrap.TextWrapper(
+            width = width,
+            break_long_words = False,
+            break_on_hyphens = False,
+        )
 
-    def at_least_one(self, offset=0):
-        "How to determine a blank line vs out of lines?"
-        index = self.n + offset
-        if index >= len(self.lines):
-            return False
-        if len(self.lines[index].strip()) == 0
-            return False
-        return True
-
-    def at_least_two(self):
-        return at_least_one() and at_least_one(1)
-
-    def blank_line(self):
-        return not at_least_one()
-
-    def absorb(self, num_of_lines):
-        for i in range(num_of_lines):
-            if self.n < len(self.lines):
-                self.result.append(self.lines[self.n])
-                self.n += 1
-
-    def skip_blank_line(self):
-        if not self.at_least_one():
-            self.absorb(1)
-            return True
+    def reformat_document(self):
+        # Chain-of-responsibility parser:
+        while not self.eof():
+            if self.skip_marked_line(): continue
+            if self.skipsubhead(): continue
+            if self.skiplisting(): continue
+            if self.skiptable(): continue
+            if self.skip_blank_lines(): continue
+            if self.reformat_paragraph(): continue
+            raise ValueError("Illegal parser state")
 
     def skip_marked_line(self):
-        if self.at_least_one() and self.lines[self.n][0] in marker_chars:
-            self.absorb(1)
+        if self.at_least_one() and self[self.n][0] in marker_chars:
+            self.transfer(1)
             return True
         return False
 
     def skipsubhead(self):
         if not self.at_least_two():
             return False
-        if self.lines[self.n][0] in subhead_chars and self.lines[self.n + 1][0] in "-=":
-            self.absorb(2)
+        if self[self.n][0] in subhead_chars and self[self.n + 1][0] in "-=":
+            self.transfer(2)
             return True
         return False
 
     def skiplisting(self):
-        "skip anything marked as a code listing"
-        if self.lines[self.n].startswith("```"):
-            self.absorb(1)
-            while not self.lines[self.n].startswith("```"):
-                self.absorb(1)
-            self.absorb(1)
+        "Skip anything marked as a code listing"
+        if self.line().startswith("```"):
+            self.transfer(1)
+            while not self.line().startswith("```"):
+                self.transfer(1)
+            self.transfer(1) # for closing ```
             return True
         return False
 
     def skiptable(self):
-        "skip a markdown table"
-        if self.blank_line(): # followed by
-            if at_least_one(1):
-                if self.lines[self.n + 1].startswith("+-"):
+        "Skip a markdown table"
+        if self.blank_line() and self.next_line().startswith("+-"):
+            self.transfer(2)
+            while self.line().startswith(("|", "+")):
+                self.transfer(1)
+            return True
+        return False
 
+    def skip_blank_lines(self):
+        if self.nonblank():
+            return False
+        while self.blank():
+            self.transfer()
+        return True
 
-    def start(self):
-        while self.n < len(self.lines):
-            if self.skip_marked_line(): continue
-            if self.skipsubhead(): continue
-            if self.skiplisting(): continue
-            if self.skiptable(): continue
-            if self.skip_blank_line(): continue
-
-
+    def reformat_paragraph(self):
+        "Reformat a single normal prose markdown paragraph"
+        if self.blank():
+            return False
+        text = ""
+        while self.nonblank() and not self.eof():
+            text += self.line() + " "
+            self.increment()
+        # Remove double spaces:
+        while text.find("  ") is not -1:
+            text = text.replace("  ", " ")
+        # Remove spaces around hyphens:
+        while text.find("- ") is not -1:
+            text = text.replace("- ", "-")
+        while text.find(" -") is not -1:
+            text = text.replace(" -", "-")
+        self.result.append(self.formatter.fill(text))
+        return True
 
 
 test = """
