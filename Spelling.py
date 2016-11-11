c722fde8761a4942435a4e3b6f2bcd6623c21614
Bruce Eckel <BruceTEckel@gmail.com>
2016-11-11 14:09:24 -0800

    Spell checking now in good shape

--- Spelling.py
+++ Spelling.py
@@ -13,6 +13,8 @@ from betools import CmdLine
 from ebook_build import *
 import config
 
+# print(combined.encode("windows-1252"))
+
 
 def filter_out_code(text):
     result = []
@@ -37,14 +39,11 @@ def spell_combined_files():
     combine_markdown_files(config.combined_markdown)
     combined = filter_out_code(config.combined_markdown.read_text(encoding="utf-8"))
     config.stripped_for_spelling.write_text(combined + "\n", encoding="utf8")
-    # os.system("subl {}".format(config.stripped_for_spelling))
     os.chdir(str(config.build_dir))
-    # os.system("bash -c aspell {}".format(config.stripped_for_spelling.name))
-
+    print("now run sp.bat")
 
 
 if __name__ == '__main__':
     CmdLine.run()
 
 
-# print(combined.encode("windows-1252"))
---
+++ sp.bat
@@ -0,0 +1,2 @@
+cd %EXTRACTED_EXAMPLES%\..\ebook_build
+bash -c "aspell check onjava-stripped-for-spelling.md"
