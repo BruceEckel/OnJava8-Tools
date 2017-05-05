252f6ad5fe45b5eeafe56ef7d4620635e1b5999c
Bruce Eckel <mindviewinc@gmail.com>
2017-05-05 15:33:22 -0600

    On the way to the solution

--- _linewidth.py
+++ _linewidth.py
@@ -5,6 +5,8 @@ Tools for checking and fixing line widths within combined markdown file
 # from pathlib import Path
 import os
 import config
+from pathlib import Path
+import textwrap
 from betools import CmdLine
 
 assert config.build_dir.exists()
@@ -50,6 +52,56 @@ def check_listing_widths():
             print("{}: {}".format(n, line))
             os.system("subl {}:{}".format(config.combined_markdown, n + 1))
             return
+
+
+# Format output:
+# (0) Do first/last lines before formatting to width
+# (1) Combine output and error (if present) files
+# (2) Format all output to width limit
+# (3) Add closing '*/'
+
+def fill_to_width(text):
+    result = ""
+    for line in text.splitlines():
+        result += textwrap.fill(line, width=config.code_width) + "\n"
+    return result.strip()
+
+def adjust_lines(text):
+    text = text.replace("\0", "NUL")
+    lines = text.splitlines()
+    slug = lines[0]
+    if "(First and Last " in slug:
+        num_of_lines = int(slug.split()[5])
+        adjusted = lines[:num_of_lines + 1] +\
+            ["...________...________...________...________..."] +\
+            lines[-num_of_lines:]
+        return "\n".join(adjusted)
+    elif "(First " in slug:
+        num_of_lines = int(slug.split()[3])
+        adjusted = lines[:num_of_lines + 1] +\
+            ["                  ..."]
+        return "\n".join(adjusted)
+    else:
+        return text
+
+
+@CmdLine("f")
+def reformat_runoutput_files():
+    "Produce .p1 files that are formatted output"
+    out_files = list(Path(".").rglob("*.out"))
+    if len(out_files) < 10:
+        print("Error: found less than 10 .out files")
+        sys.exit(1)
+    for outfile in out_files:
+        out_text = adjust_lines(outfile.read_text())
+        phase_1 = outfile.with_suffix(".p1")
+        with phase_1.open('w') as phs1:
+            phs1.write(fill_to_width(out_text) + "\n")
+            errfile = outfile.with_suffix(".err")
+            if errfile.exists():
+                phs1.write("___[ Error Output ]___\n")
+                phs1.write(fill_to_width(errfile.read_text()) + "\n")
+            phs1.write("*/\n")
 
 
 if __name__ == '__main__':
--- _update_extracted_example_output.py
+++ _update_extracted_example_output.py
@@ -1,10 +1,7 @@
 #! py -3
 # Requires Python 3.5
 # Updates generated output into extracted Java programs in "On Java 8"
-'''
-Intended to be copied into the ExtractedExamples directory,
-thus it doesn't use config.py
-'''
+# NOTE: output comes from .p1 files, not from .out files
 import sys
 from pathlib import Path
 
@@ -37,5 +34,10 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         update_file(Path(sys.argv[1]))
     else:
-        for outfile in Path(".").rglob("*.p1"):
+        p1_files = list(Path(".").rglob("*.p1"))
+        if len(p1_files) < 10:
+            print("Error: found less than 10 .p1 files")
+            sys.exit(1)
+        for outfile in p1_files: # Note p1 files have been line-wrapped
+            print(outfile)
             update_file(outfile)
