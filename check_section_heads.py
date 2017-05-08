751ddb68ce120620880b7f944988804288ad245b
Bruce Eckel <mindviewinc@gmail.com>
2017-05-08 10:19:39 -0600

    some cleanup

--- check_section_heads.py
+++ check_section_heads.py
@@ -1,9 +1,11 @@
-from pathlib import Path
-import sys
-import shutil
-import config
-import re
 import os
+import re
+import shutil
+import sys
+from pathlib import Path
+
+import config
+
 
 def say(str):
     try:
@@ -13,23 +15,26 @@ def say(str):
 
 
 printed = []
+
+
 def print_once(arg):
     if arg in printed:
         return
     printed.append(arg)
     print(arg)
 
+
 if __name__ == '__main__':
     for md in config.markdown_dir.glob("[0-9][0-9]_*.md"):
         lines = md.read_text(encoding="utf-8").splitlines()
         for n, line in enumerate(lines):
             if re.match("={6,}", line) or re.match("-{6,}", line):
-                if re.search("\d+", lines[n-1]):
+                if re.search("\d+", lines[n - 1]):
                     continue
-                if not re.search("\w+", lines[n-1]):
+                if not re.search("\w+", lines[n - 1]):
                     continue
-                if len(line) != len(lines[n-1]):
+                if len(line) != len(lines[n - 1]):
                     os.system("subl {}:{}".format(md, n))
                     print_once(md.name)
-                    say(lines[n-1])
+                    say(lines[n - 1])
                     say(line)
