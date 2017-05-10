04843f380a7383e5598a2e7d49994a1fd7be7813
Bruce Eckel <BruceTEckel@gmail.com>
2017-05-10 06:30:53 -0600

    Added match_error_output_with_tags

--- _output_file_check.py
+++ _output_file_check.py
@@ -1,22 +1,47 @@
 # Check the .out files resulting from 'gradlew run'
-'''
-Intended to be copied into the ExtractedExamples directory,
-thus it doesn't use config.py
-'''
 import pprint
 from pathlib import Path
 
-base = "/* Output:"
-example_dir = Path(".")
+from betools import CmdLine
 
-if __name__ == '__main__':
+import config
+
+
+@CmdLine("e")
+def match_error_output_with_tags():
+    """
+    Find ___[ Error Output ]___ without {ThrowsException} 
+    or {ErrorOutputExpected}, and vice-versa
+    """
+    tag_names = "{ThrowsException} or {ErrorOutputExpected}"
+    for java in config.example_dir.rglob("*.java"):
+        code = java.read_text()
+        short_path = java.relative_to(config.example_dir)
+        if "___[ Error Output ]___" in code:
+            if not ("{ThrowsException}" in code or "{ErrorOutputExpected}" in code):
+                print("{}:\n\t[Error Output] without {}\n".format(
+                    short_path, tag_names))
+        if "{ThrowsException}" in code or "{ErrorOutputExpected}" in code:
+            if not "___[ Error Output ]___" in code:
+                print("{}:\n\t{} without [Error Output]\n".format(
+                    short_path, tag_names))
+
+
+@CmdLine("o")
+def show_comment_output_tag_lines():
+    "Show /* Output: (*) in gradlw-run .out files"
     output_lines = set()
-    for md in example_dir.glob("**/*.out"):
+    base = "/* Output:"
+    for md in config.example_dir.rglob("*.out"):
         output_ln = md.read_text().splitlines()[0]
         output_lines.add(output_ln)
         if(not output_ln.startswith(base)):
-            print(str(md.relative_to(example_dir)))
+            print(str(md.relative_to(config.example_dir)))
         if output_ln.strip() != base:
-            print(str(md.relative_to(example_dir)))
+            print(str(md.relative_to(config.example_dir)))
             print(output_ln)
     pprint.pprint(output_lines)
+
+
+if __name__ == '__main__':
+    CmdLine.run()
