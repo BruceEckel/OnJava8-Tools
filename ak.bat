44e5d169d91b7a8d7694427774a5e67b45683301
Bruce Eckel <BruceTEckel@gmail.com>
2017-08-03 16:04:13 -0600

    Moved to 'tools' repo

--- ak.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if exist %GIT_HOME%\AtomicKotlin\ExtractedExamples (
-  cd %GIT_HOME%\AtomicKotlin\ExtractedExamples
-) else (
-  cd %GIT_HOME%\AtomicKotlin
-)
-%GIT_HOME%\BookBuilder\virtualenv\Scripts\activate.bat
--- git-all.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-py -3 %~dp0git-all.py %*
