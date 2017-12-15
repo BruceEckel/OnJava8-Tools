f7a7f69930a8edf8057c73154b15f088624ceed0
Bruce Eckel <mindviewinc@gmail.com>
2017-12-14 17:27:59 -0700

    Begin auto-creation of tasks.gradle

--- Examples.py
+++ Examples.py
@@ -109,6 +109,61 @@ def copyGradleFiles():
             shutil.copy(str(gradle_path), str(dest))
 
 
+def make_task(task_name, package_name = None):
+    if package_name:
+        main = "{}.{}".format(package_name, task_name)
+    else:
+        main = task_name
+    return task_name, f"""
+task {task_name}(type: JavaExec) {{
+    classpath javaClassPath
+    main = '{main}'
+}}
+"""
+
+
+@CmdLine("k")
+def createTasks():
+    tasks = """
+def javaClassPath = sourceSets.main.runtimeClasspath
+
+"""
+    task_dict = {}
+    print("Creating tasks.gradle ...")
+    print(config.example_dir)
+    if not config.example_dir.exists():
+        print("Cannot find", config.example_dir)
+        sys.exit()
+    for java_file in config.example_dir.rglob("*.java"):
+        text = java_file.read_text()
+        lines = text.splitlines()
+        if not re.search("public\s+static\s+void\s+main", text):
+            continue
+        package_name = None
+        for line in lines:
+            if line.startswith("package "):
+                package_name = line.split()[1][:-1]
+        k, v = make_task(java_file.stem, package_name)
+        task_dict[k] = v
+    for k in sorted(task_dict):
+        tasks += task_dict[k]
+    tasks += """
+task run (dependsOn: [
+"""
+    for k in sorted(task_dict):
+        tasks += f"    '{k}',\n"
+    tasks = tasks[:-1] # Strip last comma
+    tasks += """
+    ]) {
+    doLast {
+        println '*** run complete ***'
+    }
+}
+"""
+    (config.example_dir / "gradle" / "tasks.gradle").write_text(tasks)
+    print(f"{len(task_dict)} tasks")
+
+
 @CmdLine("c")
 def clean():
     "Remove ExtractedExamples directory"
